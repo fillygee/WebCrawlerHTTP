f3a2a887b5fe31ee138e7f18d0a654c22657d5e8
filomena <filomena.francisco@outlook.com>
2023-08-07 16:12:29 +0100

    wrote getURLsFromHTML test

--- crawl.js
+++ crawl.js
@@ -1,3 +1,32 @@
+const {JSDOM} = require('jsdom') 
+
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+  const linkElements =  dom.window.document.querySelectorAll('a')
+  for (const linkElement of linkElements){
+    if (linkElement.href.slice(0,1) === '/'){
+        //relative  
+        try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`)
+        urls.push(urlObj.href)
+        } catch (err){
+            console.log(`error with relative url: ${err.message} `)
+        }
+    } else {
+        try {
+            const urlObj = new URL(linkElement.href)
+            urls.push(urlObj.href)
+            } catch (err){
+                console.log(`error with absolute url: ${err.message} `)
+            }
+    }
+    
+  }
+    return urls
+}
+
+
 function normaliseURL(urlString){
     const urlObj = new URL(urlString)
     
@@ -9,6 +38,16 @@ function normaliseURL(urlString){
 }
 
 
+
+
+
+
+
+
+
+
+
 module.exports = {
-    normaliseURL
+    normaliseURL,
+    getURLsFromHTML
 }
